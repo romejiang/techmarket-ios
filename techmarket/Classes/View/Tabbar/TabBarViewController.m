6f6516c95c9484e8c10f51212f85bbdc35b8fca3
zhaojing <zhaojing19890531@163.com>
2013-07-03 17:23:44 +0800

    可变的Frame

--- techmarket/Classes/View/Tabbar/TabBarViewController.m
+++ techmarket/Classes/View/Tabbar/TabBarViewController.m
@@ -6,6 +6,8 @@
 //
 //
 
+
+#define kTabBarHeight								60
 #import "TabBarViewController.h"
 #import "TabbarView.h"
 #import <Cordova/CDVViewController.h>
@@ -17,10 +19,20 @@
 @property (strong,nonatomic)CDVViewController *firstViewController;
 @property (strong, nonatomic)CDVViewController *secondViewController;
 
+//解决下移问题(存放CDv)
+@property (strong,nonatomic)UIView*      customView;
 
 @end
 
 @implementation TabBarViewController
+
+/**************************************************************************************/
+
+#pragma mark -
+#pragma mark 公有
+#pragma mark -
+
+/**************************************************************************************/
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,24 +43,36 @@
     return self;
 }
 
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [UIApplication sharedApplication].statusBarHidden = NO;
+
+//     [self willRotateToInterfaceOrientation:self.interfaceOrientation duration:0];
+
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
+    //customView init
+    _customView = [[UIView alloc]init];
     
-    _tabBarView = [[TabbarView alloc]initWithFrame:CGRectMake(0, 420, 320, 60)];
-    
-    _tabBarView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
-    
+    [self.view addSubview:_customView];
+        
+    //tabBarView init
+    _tabBarView = [[TabbarView alloc]init];
+        
     _tabBarView.delegate = self;
     
-    [self.view addSubview:_tabBarView];
+    [_customView addSubview:_tabBarView];
     
     [self getViewControllers];
     
     for (UIViewController * viewController in _arrayViewController)
     {
-        [self.view insertSubview:viewController.view belowSubview:_tabBarView];
+        [_customView insertSubview:viewController.view belowSubview:_tabBarView];
     }
     
     [self touchBtnAtIndex:0];
@@ -59,22 +83,85 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    
-    
+      
 }
 
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+//    return NO;
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+-(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    //位置调整 （ViewAppear中调用过）
+    
+    int width  = 0;
+    
+    int height = 0;
+    
+    CGSize sizeDevice = [UIScreen mainScreen].bounds.size;
+    
+//    //横屏判断
+//    if (UIInterfaceOrientationIsPortrait(toInterfaceOrientation))
+//    {
+        width = sizeDevice.width;
+        
+        height = sizeDevice.height;
+//    }
+//    else if (UIInterfaceOrientationIsLandscape(toInterfaceOrientation))
+//    {
+//       width = sizeDevice.height;
+//       
+//       height = sizeDevice.width;
+//    }
+
+    CGFloat statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+   
+    _customView.frame = CGRectMake(0, 0, width, height);
+    
+    _tabBarView.frame = CGRectMake(0, height-20 -kTabBarHeight, width, kTabBarHeight);
+    
+    for (UIViewController *viewConroller in _arrayViewController)
+    {
+        viewConroller.view.frame = CGRectMake(0, 0, width, height-kTabBarHeight-20);
+        
+        NSLog(@"viewConroller.view.frame.origin.y = %1.2f",viewConroller.view.frame.origin.y);
+    
+    }
+    
+
+}
+
+/**************************************************************************************/
+
+#pragma mark -
+#pragma mark tabbarDelegate
+#pragma mark -
+
+/**************************************************************************************/
 
 -(void)touchBtnAtIndex:(NSInteger)index
 {
+    //根据用户点击tabbar控制View显示和隐藏
     for (UIViewController *viewController in _arrayViewController)
     {
         viewController.view.hidden = YES;
     }
     
     UIViewController *viewTouchController = [_arrayViewController objectAtIndex:index];
+    
     viewTouchController.view.hidden = NO;
- }
+}
 
+/**************************************************************************************/
+
+#pragma mark -
+#pragma mark 私有 初始化Controller
+#pragma mark -
+
+/**************************************************************************************/
 
 -(void)getViewControllers
 {
@@ -82,25 +169,15 @@
     _firstViewController.wwwFolderName = @"www";
     _firstViewController.startPage = @"spec.html";
     _firstViewController.useSplashScreen = NO;
-    _firstViewController.view.frame = CGRectMake(0, 0, 320, 480);
+//    _firstViewController.view.frame = CGRectMake(0, 0, 320, 480);
     
     _secondViewController = [CDVViewController new];
     _secondViewController.wwwFolderName = @"www";
     _secondViewController.startPage = @"index.html";
     _secondViewController.useSplashScreen = NO;
-    _secondViewController.view.frame = CGRectMake(0, 0, 320, 480);
+//    _secondViewController.view.frame = CGRectMake(0, 0, 320, 480);
     
     _arrayViewController = [NSArray arrayWithObjects:_firstViewController,_secondViewController,_firstViewController,_secondViewController, nil];
-    
-//    aaViewController *aa = [[aaViewController alloc]init];
-//    aa.view.frame = CGRectMake(0, 0, 320, 480);
-//    
-//    bbViewController *bb = [[bbViewController alloc]init];
-//    bb.view.frame =CGRectMake(0, 0, 320, 480);
-//    ccViewController *cc = [[ccViewController alloc]init];
-//    cc.view.frame = CGRectMake(0, 0, 320, 480);
-    
-//    _arrayViewController = [NSArray arrayWithObjects:aa,bb,cc,cc, nil];
     
 }
 
