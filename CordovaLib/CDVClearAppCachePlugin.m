e6abec9dcb0f685e4a2526d8ad7998ea0adcc51a
zhaojing <zhaojing19890531@163.com>
2013-09-24 15:55:15 +0800

    为清理缓存加蒙版

--- CordovaLib/CDVClearAppCachePlugin.m
+++ CordovaLib/CDVClearAppCachePlugin.m
@@ -16,6 +16,8 @@
 
 @property (strong, nonatomic) ActivityIndicatorView *viewActivityIndicatorView;
 
+@property (strong, nonatomic) UIControl*             overlayer;
+
 @end
 
 @implementation CDVClearAppCachePlugin
@@ -26,6 +28,14 @@
     _viewActivityIndicatorView = [[ActivityIndicatorView alloc]initWithFrame:CustomActivity_IndicatorViewFrame];
     [_viewActivityIndicatorView setLabelTextWithContent:@"正在清理缓存"];
     [self.viewController.view addSubview:_viewActivityIndicatorView];
+
+    self.overlayer = [[UIControl alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    self.overlayer.backgroundColor = [UIColor colorWithRed:.16 green:.17 blue:.21 alpha:0.6];
+    [self.overlayer addTarget:self
+                       action:@selector(touch)
+             forControlEvents:UIControlEventTouchUpInside];
+    [self.viewController.view addSubview:self.overlayer];
+    self.overlayer.hidden = YES;
 }
 
 
@@ -33,15 +43,26 @@
 {
     [_viewActivityIndicatorView startAnimation];
     
+    self.overlayer.hidden = NO;
+    
     [self performSelector:@selector(stopClearCache) withObject:self afterDelay:10.0];
 }
 
 -(void)stopClearCache
 {
     [_viewActivityIndicatorView stopAnimation];
+    
+    self.overlayer.hidden = YES;
+    
     [KGStatusBar showSuccessWithStatus:@"清理缓冲成功"];
+    
 
 }
 
+-(void)touch
+{
+
+
+}
 
 @end
